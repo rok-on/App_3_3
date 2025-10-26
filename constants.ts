c974f4737ac9a9d5ca8458ee589eb702d80fdfa8
rok-on <84695130+rok-on@users.noreply.github.com>
2025-10-26 16:29:01 +0100

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,13 +1,11 @@
-
 import type { User } from './types';
 
-export const USERS: User[] = ['Vid', 'Leonard', 'Mark', 'Tilen'];
+export const USERS: User[] = ['Vid', 'Mark', 'Tilen'];
 
 export const TASK_ONE_ANSWERS: Record<User, number> = {
-  Vid: 4,
-  Leonard: 1,
-  Mark: 3,
-  Tilen: 2,
+  Vid: 3,
+  Mark: 2,
+  Tilen: 1,
 };
 
 export const TASK_TWO_ROUNDS = 30;
